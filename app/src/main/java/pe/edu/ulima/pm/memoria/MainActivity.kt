2550cc00bb6b3255151e4d5713159d67f0eb9a02
alvaroenrique <alvaroenriquehl@gmail.com>
2020-04-29 15:38:34 -0500

    Initial commit

---
+++ app/src/main/java/pe/edu/ulima/pm/memoria/MainActivity.kt
@@ -0,0 +1,12 @@
+package pe.edu.ulima.pm.memoria
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
