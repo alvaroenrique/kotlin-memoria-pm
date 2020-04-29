f86534d52c6734fdaff1a3fd8e923d08f23f9bdd
alvaroenrique <alvaroenriquehl@gmail.com>
2020-04-29 17:55:59 -0500

    Agregado función initGame

--- app/src/main/java/pe/edu/ulima/pm/memoria/MainActivity.kt
+++ app/src/main/java/pe/edu/ulima/pm/memoria/MainActivity.kt
@@ -12,18 +12,38 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        initGame()
     }
 
-    fun butOnClick(view: View) {
-        val but: Button = view as Button
+    fun initGame() {
+        val emojis: Array<String> = arrayOf(
+            "\uD83D\uDE00",
+            "\uD83D\uDE03",
+            "\uD83D\uDE04",
+            "\uD83D\uDE01",
+            "\uD83D\uDE06",
+            "\uD83D\uDE05"
+        )
 
-        if (but.text==""){
-            but.text = "\uD83D\uDE00"
+        val emejisToAsign = emojis + emojis
+
+        val buttons: Array<Button> = arrayOf(
+            findViewById<Button>(R.id.but1),
+            findViewById<Button>(R.id.but2),
+            findViewById<Button>(R.id.but3),
+            findViewById<Button>(R.id.but4),
+            findViewById<Button>(R.id.but5),
+            findViewById<Button>(R.id.but6),
+            findViewById<Button>(R.id.but7),
+            findViewById<Button>(R.id.but8),
+            findViewById<Button>(R.id.but9),
+            findViewById<Button>(R.id.but10),
+            findViewById<Button>(R.id.but11),
+            findViewById<Button>(R.id.but12)
+        )
+
+        buttons.forEachIndexed { index, element ->
+            element.text = emejisToAsign[index]
         }
-        else {
-            but.text = ""
-        }
-        Log.i("HolaAndroid", "Me hizo click en el boton" + but.text)
     }
 }
